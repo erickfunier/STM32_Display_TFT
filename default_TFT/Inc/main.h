80d3e9ea6ad34258fe6a81518ebb756bc48fc8db
Erick Funier <erickfuniers@gmail.com>
2019-01-24 10:04:15 -0200

    add comments to code

--- default_TFT/Inc/main.h
+++ default_TFT/Inc/main.h
@@ -50,36 +50,36 @@
 
 /* Private define ------------------------------------------------------------*/
 
-#define LED_Pin GPIO_PIN_13
-#define LED_GPIO_Port GPIOC
-#define XTAL_Pin GPIO_PIN_14
-#define XTAL_GPIO_Port GPIOC
-#define LCD_RD_Pin GPIO_PIN_0
-#define LCD_RD_GPIO_Port GPIOA
-#define LCD_WR_Pin GPIO_PIN_1
-#define LCD_WR_GPIO_Port GPIOA
-#define LCD_RS_Pin GPIO_PIN_2
-#define LCD_RS_GPIO_Port GPIOA
-#define LCD_CS_Pin GPIO_PIN_3
-#define LCD_CS_GPIO_Port GPIOA
-#define LCD_RST_Pin GPIO_PIN_4
-#define LCD_RST_GPIO_Port GPIOA
-#define LCD_D0_Pin GPIO_PIN_10
-#define LCD_D0_GPIO_Port GPIOB
-#define LCD_D1_Pin GPIO_PIN_11
-#define LCD_D1_GPIO_Port GPIOB
-#define LCD_D2_Pin GPIO_PIN_4
-#define LCD_D2_GPIO_Port GPIOB
-#define LCD_D3_Pin GPIO_PIN_5
-#define LCD_D3_GPIO_Port GPIOB
-#define LCD_D4_Pin GPIO_PIN_6
-#define LCD_D4_GPIO_Port GPIOB
-#define LCD_D5_Pin GPIO_PIN_7
-#define LCD_D5_GPIO_Port GPIOB
-#define LCD_D6_Pin GPIO_PIN_8
-#define LCD_D6_GPIO_Port GPIOB
-#define LCD_D7_Pin GPIO_PIN_9
-#define LCD_D7_GPIO_Port GPIOB
+#define LED_Pin GPIO_PIN_13 // LED onboard of bluepill
+#define LED_GPIO_Port GPIOC // LED onboard of bluepill
+#define XTAL_Pin GPIO_PIN_14 // Crystal Osclillator 8 MHz onboard of bluepill
+#define XTAL_GPIO_Port GPIOC // Crystal Osclillator 8 MHz onboard of bluepill
+#define LCD_RD_Pin GPIO_PIN_0 // RD PIN of LCD mcufriend connected on PA0 of bluepill
+#define LCD_RD_GPIO_Port GPIOA // RD PIN of LCD mcufriend connected on PA0 of bluepill
+#define LCD_WR_Pin GPIO_PIN_1 // WR PIN of LCD mcufriend connected on PA1 of bluepill
+#define LCD_WR_GPIO_Port GPIOA // WR PIN of LCD mcufriend connected on PA1 of bluepill
+#define LCD_RS_Pin GPIO_PIN_2 // RS PIN of LCD mcufriend connected on PA2 of bluepill
+#define LCD_RS_GPIO_Port GPIOA // RS PIN of LCD mcufriend connected on PA2 of bluepill
+#define LCD_CS_Pin GPIO_PIN_3 // CS PIN of LCD mcufriend connected on PA3 of bluepill
+#define LCD_CS_GPIO_Port GPIOA // CS PIN of LCD mcufriend connected on PA3 of bluepill
+#define LCD_RST_Pin GPIO_PIN_4 // RST PIN of LCD mcufriend connected on PA4 of bluepill
+#define LCD_RST_GPIO_Port GPIOA // RST PIN of LCD mcufriend connected on PA4 of bluepill
+#define LCD_D0_Pin GPIO_PIN_10 // LCD D0 PIN of LCD mcufriend connected on PB10 of bluepill
+#define LCD_D0_GPIO_Port GPIOB // LCD D0 PIN of LCD mcufriend connected on PB10 of bluepill
+#define LCD_D1_Pin GPIO_PIN_11 // LCD D1 PIN of LCD mcufriend connected on PB11 of bluepill
+#define LCD_D1_GPIO_Port GPIOB // LCD D1 PIN of LCD mcufriend connected on PB11 of bluepill
+#define LCD_D2_Pin GPIO_PIN_4 // LCD D2 PIN of LCD mcufriend connected on PB4 of bluepill
+#define LCD_D2_GPIO_Port GPIOB // LCD D2 PIN of LCD mcufriend connected on PB4 of bluepill
+#define LCD_D3_Pin GPIO_PIN_5 // LCD D3 PIN of LCD mcufriend connected on PB5 of bluepill
+#define LCD_D3_GPIO_Port GPIOB // LCD D3 PIN of LCD mcufriend connected on PB5 of bluepill
+#define LCD_D4_Pin GPIO_PIN_6 // LCD D4 PIN of LCD mcufriend connected on PB6 of bluepill
+#define LCD_D4_GPIO_Port GPIOB // LCD D4 PIN of LCD mcufriend connected on PB6 of bluepill
+#define LCD_D5_Pin GPIO_PIN_7 // LCD D5 PIN of LCD mcufriend connected on PB7 of bluepill
+#define LCD_D5_GPIO_Port GPIOB // LCD D5 PIN of LCD mcufriend connected on PB7 of bluepill
+#define LCD_D6_Pin GPIO_PIN_8 // LCD D6 PIN of LCD mcufriend connected on PB8 of bluepill
+#define LCD_D6_GPIO_Port GPIOB // LCD D6 PIN of LCD mcufriend connected on PB8 of bluepill
+#define LCD_D7_Pin GPIO_PIN_9 // LCD D7 PIN of LCD mcufriend connected on PB9 of bluepill
+#define LCD_D7_GPIO_Port GPIOB // LCD D7 PIN of LCD mcufriend connected on PB9 of bluepill
 
 /* ########################## Assert Selection ############################## */
 /**
@@ -91,18 +91,18 @@
 /* USER CODE BEGIN Private defines */
 
 
-int flag_adc;
-uint32_t val_adc1; // valor lido no conv ADC
-uint32_t val_adc2; // valor lido no conv ADC
-uint32_t val_adc3; // valor lido no conv ADC
+int flag_adc; // flag end of conversion
+uint32_t val_adc1; // read result of conv ADC
+uint32_t val_adc2; // read result of conv ADC
+uint32_t val_adc3; // read result of conv ADC
 
-void wr_analog_input();
-void rs_cmd();
-void rs_analog_input();
-void wr_idle();
-void wr_active();
+void wr_analog_input(); // function to set WR PIN (connected to WR PIN of LCD) analog for read touch
+void rs_cmd();	// function to set RS PIN to send command to LCD
+void rs_analog_input(); // function to set RS PIN (connected to RS PIN of LCD) analog for read touch
+void wr_idle();	// function to set WR PIN like idle for LCD
+void wr_active();	// function to set WR PIN like active for LCD
 
-void startADC1IT();
+void startADC1IT();	// start ADC 1 Interruption for read touch values from LCD
 
 /* USER CODE END Private defines */
 
