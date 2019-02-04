d29ec3737885a28520496a8401829849b6fb1b7a
Erick Funier <erickfuniers@gmail.com>
2019-02-04 11:38:56 -0200

    Removed unused functions and inserted comments

--- default_TFT/Inc/touch.h
+++ default_TFT/Inc/touch.h
@@ -3,21 +3,16 @@
  *
  *  Created on: 14 de jan de 2019
  *      Author: Inspiron
+ *
+ *
  */
 
 #ifndef TOUCH_H_
 #define TOUCH_H_
 
-#define MINPRESSURE 200
-#define MAXPRESSURE 1000
-
-int touchx_atual;
-int touchy_atual;
-int touchz_atual;
-
-void readTouchX();
-void readTouchY();
-void readTouchZ();
-void readTouch();
+void readTouch();	// Read touch x and y and validate touch
+void readTouchX();	// Read touch x position on screen
+void readTouchY();	// Read touch y position on screen
+void readTouchZ();	// Read touch "z" pressed on screen
 
 #endif /* TOUCH_H_ */
